86d4fc8a9467dc755bd1b56bc9f588e4353c3394
Brett <brettatoms@gmail.com>
2011-12-24 12:32:39 -0500

    - make setup.py work

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
+import os
+import sys
+
 usage = """setup.py <command>
-  - upload: upload new data files
-  - deploy: update application at appengine server
-  - clear_datastore: clear the production datastore
+  - clear: remove unnecessary files
 """
 
+def clean():
+    patterns = ['MANIFEST', '*~', '*flymake*', '*.pyc', '*.h']
+    cwd = os.getcwd()
+    import fnmatch
+    for path, subdirs, files in os.walk(cwd):
+        for pattern in patterns:
+            matches = fnmatch.filter(files, pattern)
+            if matches:
+                def delete(p):
+                    print 'removing %s' % p
+                    os.remove(p)
+                map(delete ,[os.path.join(path, m) for m in matches])
+
 if __name__ == '__main__':
-    actions = {'upload': upload,
-               'deploy': deploy,
-               'update': deploy,
-               'clear_datastore': clear_datastore}
+    actions = {'clean': clean}
 
     if len(sys.argv) < 2 or sys.argv[1] not in actions:
         print usage
         sys.exit(1)
-    
+
     actions[sys.argv[1]]()
